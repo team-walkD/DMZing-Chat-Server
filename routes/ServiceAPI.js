85b0004a821651f2ca9a4059c1c37090c67376ad
Kiyong <gy8971@gmail.com>
2018-11-01 15:12:19 +0900

    Set Develop Environment

--- routes/ServiceAPI.js
+++ routes/ServiceAPI.js
@@ -1,7 +1,5 @@
 'use strict';
 
-const imageUtil = require('../ImageUtil');
-
 const authCtrl = require('../controllers/AuthCtrl');
 const userCtrl = require('../controllers/UserCtrl');
 const doboSTLCtrl = require('../controllers/DoboSLTCtrl');
@@ -10,17 +8,12 @@ const doboCtrl = require('../controllers/DoboCtrl');
 module.exports = (router) => {
 
   // USER
-  router.route('/users/signup')
-    .post(imageUtil.uploadSingle, userCtrl.signup);
-
   router.route('/users/signin')
     .post(userCtrl.signin);
 
   router.route('/users/pwd') //정보 수정
     .put(authCtrl.auth, userCtrl.editPwd);
 
-  router.route('/users/avatar') //사진 수정
-    .put(authCtrl.auth, imageUtil.uploadSingle, userCtrl.editAvatar);
 
   router.route('/seoulight/register') //시민해설사 신청
     .post(authCtrl.auth, userCtrl.reqSeoulight);
@@ -34,8 +27,6 @@ module.exports = (router) => {
 
   // DOBO WITH SLT
 
-  router.route('/seoulite')
-    .post(authCtrl.auth, imageUtil.uploadBGI, doboSTLCtrl.register);
 
   router.route('/seoulite/:category')
     .get(authCtrl.auth, doboSTLCtrl.getList);
